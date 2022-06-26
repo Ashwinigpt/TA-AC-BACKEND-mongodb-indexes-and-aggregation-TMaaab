b1e074b0cf749a85c6eb1072f66df4813acdacfd
Ashwini Gupta <ashwinigpt1996@gmail.com>
2022-06-26 17:11:21 +0530

    block-BMaaak

--- block-BMaaak/blog/models/User.js
+++ block-BMaaak/blog/models/User.js
@@ -3,8 +3,8 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
     name: { type: String },
-    username: { type: String, unique: true },
-    email: { type: String, unique: true },
+    username: { type: String },
+    email: { type: String },
     address: {
         city: String,
         state: String,
@@ -13,6 +13,7 @@ var userSchema = new Schema({
     }
 });
 
-userSchema.index({ email: 1, username: 1 }, { unique: true }, { 'address.state': 1 , 'address.country': 1});
+userSchema.index({ email: 1, username: 1 }, { unique: true }),
+userSchema.index({ 'address.state': 1 , 'address.country': 1}, { unique: true }),
 
 module.exports = mongoose.model('User', userSchema);
